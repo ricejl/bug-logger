e7b02908516bbf02f6f8f6f345f1521e9d657c35
Jenny <ricejl@gmail.com>
2019-12-22 19:05:50 -0700

    start home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col p-0">
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            B
+            <i class="fas fa-bug"></i>g Tracker
+          </a>
+          <button
+            class="btn btn-outline-dark my-2 my-sm-0"
+            data-toggle="modal"
+            data-target="#reportBug"
+          >Report</button>
+        </nav>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col pt-4 text-left pl-4 pb-1">
+        <h5>Current Bugs</h5>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col pl-5 pr-5">
+        <table class="table table-striped table-dark text-left">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Reported by</th>
+              <th scope="col">Status</th>
+              <th scope="col">Last Modified</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- SECTION rows to be vue components -->
+            <tr>
+              <th scope="row">Acct reactivation</th>
+              <td>Mark</td>
+              <td>open</td>
+              <td>date</td>
+            </tr>
+            <tr>
+              <th scope="row">Authentication</th>
+              <td>Jacob</td>
+              <td>open</td>
+              <td>date</td>
+            </tr>
+            <tr>
+              <th scope="row">Acct Avatar</th>
+              <td>Larry</td>
+              <td>closed</td>
+              <td>date</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
